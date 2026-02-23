b52e65aab4f14596f535d140591014945bf33b79
jainsarthak456 <55425634+jainsarthak456@users.noreply.github.com>
2026-02-23 20:40:30 +0530

    Delete backend directory

--- backend/src/main/java/com/taskmanager/repository/TaskRepository.java
+++
@@ -1,4 +0,0 @@
-package com.taskmanager.repository;
-import com.taskmanager.entity.Task;
-import org.springframework.data.jpa.repository.JpaRepository;
-public interface TaskRepository extends JpaRepository<Task,Long>{}
--- backend/src/main/java/com/taskmanager/service/TaskService.java
+++
@@ -1,9 +0,0 @@
-package com.taskmanager.service;
-import com.taskmanager.entity.Task;
-import java.util.List;
-public interface TaskService{
- Task createTask(Task t);
- List<Task> getAllTasks();
- Task getTaskById(Long id);
- void deleteTask(Long id);
-}
